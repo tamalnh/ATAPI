01193e920f0689def6a4a7c137e06852e3583597
Tamal H <tamalnh@users.noreply.github.com>
2018-07-17 00:40:14 +0600

    Add files via upload

---
+++ embedding.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
+  author: {
+    type: authorSchema,
+    required: true
+  }
+}));
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name, 
+    author
+  }); 
+  
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() { 
+  const courses = await Course.find();
+  console.log(courses);
+}
+
+async function upadteAuthor(courseID) {
+  const course =  await Course.findById(courseID)
+  course.author.name = 'Tamal H';
+  course.save()
+}
+
+// createCourse('Node Course', new Author({ name: 'Mosh' }));
+upadteAuthor('5b4ce227fb08c204c0c55782');
